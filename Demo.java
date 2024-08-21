4ecc93e8625bc015bb614234fc4b312774fabe15
oliviacari <jonesoc2@appstate.edu>
2024-08-21 09:48:56 -0400

    1.0b Changed Name, added Class & Main

---
+++ Demo.java
@@ -0,0 +1,8 @@
+//Version 1.0b 8/21/2024
+import java.util.Scanner;
+//hello world 
+public class Demo{
+    public static void main(String[] args) {
+        Scanner scanboy = new Scanner(System.in);
+    }
+}
